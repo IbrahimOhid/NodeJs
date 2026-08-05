21e2cdfdffd153f4c1819f854519bf7051c0faa9
ibrahimohid <ibrahimohid77@gmail.com>
2026-08-05 10:20:36 +0600

    path module practice done

--- path_module/index.js
+++ path_module/index.js
@@ -1,2 +1,12 @@
 import path from "path";
-console.log(path)
+import { fileURLToPath } from "url";
+
+// file name
+const __filename = fileURLToPath(import.meta.url);
+// directory name
+const __dirname = path.dirname(__filename);
+// full path
+const fullPath = path.join(__dirname, "example", "file.txt");
+// get the extension name
+const extName = path.extname(fullPath);
+console.log(extName)
